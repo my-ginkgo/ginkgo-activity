5248e85ea334aa30f6f45f3e9b80ab9c834d8652
Stefano Denti <stefano@med2me.tech>
2022-11-08 22:17:25 +0100

    fix

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const GPSMetrics: {
-    calcAll: (blocks: import("./models").ActivityBlocks, settings: import("./models").ActivitySettings, userInfo: import("./models").ActivityUserInfo, type: import("./models").ActivityType) => Partial<import("./models").GpsMetrics>;
-    calcValues: (blocks: import("./models").ActivityBlocks) => Pick<import("./models").GpsMetrics, "altitudeMin" | "altitudeMax" | "totalDistance" | "speedMax" | "speedMin" | "totalTime" | "avgSpeed" | "avgAltitude">;
-    calcRanges: (blocks: import("./models").ActivityBlocks, settings: import("./models").ActivitySettings) => Partial<import("./models").GpsMetrics>;
-};
