abe42bf22ce45c61da3281f91fff72533d4724a3
Stefano Denti <stefano@med2me.tech>
2022-11-09 09:57:05 +0100

    Implementation MP metrics Service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,7 @@
 import gpsMetrics from './gps-metrics.service';
 import heartMetrics from './heart-metrics.service';
+import metabolicMetrics from './metabolic-metrics.service';
 
 export const GPS = gpsMetrics;
-
 export const HR = heartMetrics;
+export const MP = metabolicMetrics;
